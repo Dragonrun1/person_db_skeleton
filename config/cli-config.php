7cd02876f928b8f55b35e2703fd1483783545a08
Michael Cummings <dragonrun1@gmail.com>
2019-12-06 10:22:43 -0800

    Refactored cli-config.php paths etc.

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,16 +1,24 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+declare(strict_types=1);
+require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Doctrine\ORM\Tools\Setup;
 use Symfony\Component\Console\Helper\HelperSet;
 
-$paths = [dirname( __DIR__, 1) . '/src/Model/Entities'];
+$paths = [dirname(__DIR__, 1) . '/src/Model/Entities'];
 $isDevMode = true;
-$dbParams = include 'migrations-db.php';
+$dbParams = require 'migrations-db.php';
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
-$entityManager = EntityManager::create($dbParams, $config);
+try {
+    $entityManager = EntityManager::create($dbParams, $config);
+} catch (ORMException $e) {
+    print $e->getMessage() . PHP_EOL;
+    print $e->getTraceAsString();
+    exit(1);
+}
 return new HelperSet(
     [
         'em' => new EntityManagerHelper($entityManager),
