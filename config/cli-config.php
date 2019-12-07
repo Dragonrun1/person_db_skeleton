4855ecc4ee90bbe6d986fb28948dab55f0731e55
Michael Cummings <dragonrun1@gmail.com>
2019-12-07 12:23:45 -0800

    Updated licence info to correct BSD-3-Clause instead of GPL.

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,5 +1,51 @@
 <?php
 declare(strict_types=1);
+/**
+ *
+ * PHP version 7.3
+ *
+ * LICENSE:
+ * This file is part of person_db_skeleton which is a set of skeleton database
+ * tables for people and common associated data.
+ *
+ * Copyright (C) 2019 Michael Cummings. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the BSD-3 Clause License along with
+ * this program. If not, see
+ * <https://spdx.org/licenses/BSD-3-Clause.html>.
+ *
+ * You should be able to find a copy of this license in the LICENSE file.
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2019 Michael Cummings
+ * @license   BSD-3-Clause
+ */
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 use Doctrine\Common\Annotations\AnnotationException;
 use Doctrine\DBAL\DBALException;
--- src/Utils/Uuid4.php
+++ src/Utils/Uuid4.php
@@ -8,28 +8,45 @@ declare(strict_types=1);
  * LICENSE:
  * This file is part of person_db_skeleton which is a set of skeleton database
  * tables for people and common associated data.
- * Copyright (C) 2019 Michael Cummings
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, version 2 of the License.
+ * Copyright (C) 2019 Michael Cummings. All rights reserved.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
  *
- * You should have received a copy of the GNU General Public License along with
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the BSD-3 Clause License along with
  * this program. If not, see
- * <http://spdx.org/licenses/GPL-2.0.html>.
+ * <https://spdx.org/licenses/BSD-3-Clause.html>.
  *
  * You should be able to find a copy of this license in the LICENSE file.
  *
  * @author    Michael Cummings <mgcummings@yahoo.com>
  * @copyright 2019 Michael Cummings
- * @license   GPL-2.0
+ * @license   BSD-3-Clause
  */
-
 namespace PersonDBSkeleton\Utils;
 
 /**
