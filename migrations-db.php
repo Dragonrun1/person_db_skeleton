858a36c6df1e1734247266941de6887b6933c1a9
Michael Cummings <dragonrun1@gmail.com>
2019-12-16 10:43:02 -0800

    Made migrations-db.php multiple platform with Mysql default. Only Mysql and Sqlite currently implemented.

--- migrations-db.php
+++ migrations-db.php
@@ -1,11 +1,22 @@
 <?php
 declare(strict_types=1);
-return [
-    'charset' => 'utf8mb4',
-    'collate' => 'utf8mb4_general_ci',
-    'dbname' => 'person_db_skeleton',
-    'driver' => 'pdo_mysql',
-    'host' => 'localhost',
-    'password' => 'secret',
-    'user' => 'PDBSUser',
-];
+$platform = $platform ?? 'mysql';
+switch ($platform) {
+    case 'mysql':
+        return [
+            'charset' => 'utf8mb4',
+            'collate' => 'utf8mb4_general_ci',
+            'dbname' => 'person_db_skeleton',
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'password' => 'secret',
+            'user' => 'PDBSUser',
+        ];
+    case 'sqlite':
+        return [
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/person_db_skeleton.sq3',
+        ];
+    default:
+        return [];
+}
