ad4b268e339b92712e0b483aa7d04bd25812b8d0
Michael Cummings <dragonrun1@gmail.com>
2020-03-07 21:59:47 -0800

    Some minor updates to bootstrap.php file.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -9,14 +9,13 @@ declare(strict_types=1);
 /*
  * Nothing to do if Composer auto loader already exists.
  */
-/** @noinspection ClassConstantCanBeUsedInspection */
 if (\class_exists('\Composer\Autoload\ClassLoader', false)) {
     return 0;
 }
 /*
  * Find Composer auto loader after striping away any vendor path.
  */
-$path = \str_replace('\\', '/', \dirname(__DIR__, 1));
+$path = \str_replace('\\', '/', \dirname(__DIR__));
 $vendorPos = \strpos($path, 'vendor/');
 if (false !== $vendorPos) {
     $path = \substr($path, 0, $vendorPos);
@@ -30,7 +29,6 @@ $errorReporting = \error_reporting(E_ALL & ~E_WARNING);
 include_once $path;
 \error_reporting($errorReporting);
 unset($errorReporting, $path, $vendorPos);
-/** @noinspection ClassConstantCanBeUsedInspection */
 if (!\class_exists('\Composer\Autoload\ClassLoader', false)) {
     $mess = 'Could NOT find required Composer class auto loader. Aborting ...';
     if ('cli' === PHP_SAPI) {
